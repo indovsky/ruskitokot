078ca01333ee7e1413b1418c44205a6e956646d8
Maciej Tymosiak <viptymo@gmail.com>
2019-09-29 11:53:29 +0200

    Change section #2

--- inc/section-about.php
+++ inc/section-about.php
@@ -6,8 +6,8 @@
                     <h1 class="section__title">Ruski - Kim jest Ruski?</h1>
                 </div>
                 <div class="section__content">
-                    <img class="section__image section__image--small section__image--pull-right section__image--rounded" src="<?php echo getTemplateUri()->img ?>/ruskisquat.jpg" alt="Ruski">
-                    <p>Dwudziestojednoletni raper z <strong>Białegostoku</strong> tworzy muzykę już od ponad sześciu lat. Mieszkający na codzień w Brukseli, Ruski ma za sobąparę projektów takich jak : <strong>Car Dealership</strong>, <strong>Narson EP</strong>, <strong>Jesień 2017</strong> czy <strong>Lofiepe</strong>.Wszystkie jego projekty są dostępne za darmo na platformie <a href="https://www.youtube.com/channel/UCxmUUahj-R8MpIsWzrPloMQ" title="Ruski YouTube" target="_blank" rel="nofollow">YouTube</a>.</br>
+                    <img class="section__image section__image--small section__image--pull-right section__image--rounded d-none d-sm-block" src="<?php echo getTemplateUri()->img ?>/ruskisquat.jpg" alt="Ruski" title="Ruski">
+                    <p><strong>Ruski</strong> - Dwudziestojednoletni raper z <strong>Białegostoku</strong> tworzy muzykę już od ponad sześciu lat. Mieszkający na codzień w Brukseli, Ruski ma za sobąparę projektów takich jak : <strong>Car Dealership</strong>, <strong>Narson EP</strong>, <strong>Jesień 2017</strong> czy <strong>Lofiepe</strong>.Wszystkie jego projekty są dostępne za darmo na platformie <a href="https://www.youtube.com/channel/UCxmUUahj-R8MpIsWzrPloMQ" title="Ruski YouTube" target="_blank" rel="nofollow">YouTube</a>.</br>
                     26.04.2019 wychodzi kolejny krążek w duecie z 2dk czyli « FunkyMonkeys ». Dorian (bo tak ma na imię) nie boi się eksperymentować z muzyką co słychać po jego twórczości. Rozbieżność w jego muzyce jest zauważalna, każdy może znaleźć tam cośdla siebie – od chill lofi po tak zwany newschool.</p>
                 </div>
 			</div>
